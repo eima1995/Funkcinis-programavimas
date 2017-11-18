46092fa933089584fc85dc0f889c0c44be07a147
Eimantas <eimantasbendorius@example.com>
2017-11-18 13:15:58 +0200

    done task4

--- task4/Ex4.hs
+++ task4/Ex4.hs
@@ -1,48 +1,47 @@
 module Ex4
 where
 -- Ex1
-listLen :: [a] -> Int
 listLen = sum . map(\x -> 1)
 
-listLen2 :: [a] -> Int
-listLen2 = foldr (+) 0 . map(\x -> 1)
+listLen2 xs = foldr (\x -> (+1)) 0 xs
 
 -- Ex2
 any1 :: (a -> Bool) -> [a] -> Bool
-any1 p x
-    |length(filter p x) > 0 = True
-    |otherwise = False
+any1 p x = length(filter p x) > 0
 
 all1 :: (a -> Bool) -> [a] -> Bool
-all1 p x
-    |length(filter p x) == length x = True
-    |otherwise = False
+all1 p x = length(filter p x) == length x
 
 any2 p x = foldr (||) False (map p x)
-all2 p x = foldr (&&) False (map p x)
+all2 p x = foldr (&&) True (map p x)
 
 -- Ex3
-unzip1 xs = (map fst xs, map snd xs)
+unzip1 x = foldr (\(y, z) (ys, zs) -> (y:ys, z:zs)) ([], []) x
 
 -- Ex4
-ff maxNum = sum . filter(>0) . map(*10)
-
-sum1 p x = foldr (+) 0 (map p x)
+ff :: Integer -> [Integer] -> Integer
+ff num = foldr f 0 . map (*10) . filter (>=0)  
+    where 
+        f suma y
+            | suma + y <= num = y + suma
+            | otherwise = y
 
 -- Ex5
 flip1 f = \x y -> f y x  
 
 -- Ex6
+total1 f n = (sum . map f) [0..n]
 
 -- Ex7
+iter1 :: Integer -> (a->a) -> (a->a)
+iter1 n f
+  | n <= 0 = id
+  | otherwise = f . (iter1 (n-1) f)
 
+iter2 :: Integer -> (a->a) -> (a->a)
+iter2 n f = foldr (.) id (replicate (fromIntegral n) f)
 
 -- Ex8
-split1 xs = foldr f x xs
-  where
-   f (x,y)(xs,ys) = (x:xs, y:ys)
-   x = ([],[])
-
-
-
- 
+splits1 x = map f [0..length(x)]
+    where
+     f n = ((take n x), (drop n x))
